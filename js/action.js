1668e0eb729b7bde72051fdb52d358d27f48236a
w-mj <w.mingjian@yahoo.com>
2018-07-27 12:01:49 +0800

    responsive canvas

--- js/action.js
+++ js/action.js
@@ -2,6 +2,8 @@ let ctx, cvs;
 let drawing = false;
 
 $(document).ready(function() {
+    $('#canvas-container').height($("#text-window").height());
+    CanvasAutoResize.initialize();
     cvs = $("#canvas-area");
     ctx = document.getElementById('canvas-area').getContext('2d');
     ctx.lineWidth = 2;
@@ -37,3 +39,20 @@ function setLineColor(color) {
 function clearCanvas() {
     ctx.clearRect(0, 0, cvs.width(), cvs.height());
 }
+
+let CanvasAutoResize = {
+    draw: function() {
+        let ctx = document.getElementById('canvas-area').getContext('2d');
+        let canvasContainer = document.getElementById('canvas-container');
+        ctx.canvas.width  = canvasContainer.offsetWidth-2;
+        ctx.canvas.height = canvasContainer.offsetHeight-2;
+    },
+
+    initialize: function(){
+        let self = CanvasAutoResize;
+        self.draw();
+        $(window).on('resize', function(event){
+            self.draw();
+        });
+    }
+};
